f42ef93f5780a2ddf89a0b77ce6822c2979a72f3
Adrian Gonzalez Barbosa <adrian.gonzalez.barbosa@gmail.com>
2016-02-16 17:25:39 +0000

    Practica2 working

--- practica2/practica2.c
+++ practica2/practica2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "mpi.h"
 #define MAXPROC 8    /* Número mínimo de procesos */
-#define LENGTH 5  /* Longitud datos a enviar */
+#define LENGTH 3  /* Longitud datos a enviar */
 
 main(int argc, char* argv[]) {
    int i, j, numProc, miRango, rang;
@@ -16,8 +16,8 @@ main(int argc, char* argv[]) {
    MPI_Comm_rank(MPI_COMM_WORLD, &miRango); 
 
    int x[LENGTH*(numProc-1)][LENGTH];        /* Buffer envío */
-   int y[LENGTH*(numProc-1)][LENGTH];        /* Buffer recepción*/
-   int z[LENGTH*(numProc-1)][LENGTH];
+   int y[1][LENGTH];                 /* Buffer recepción*/
+   int z[numProc-1][LENGTH];
 
    MPI_Type_vector(LENGTH, LENGTH, LENGTH, MPI_INT, &Column_type);
 
@@ -48,31 +48,29 @@ main(int argc, char* argv[]) {
        }
       }
       for (rang=1; rang<numProc; rang++) {
-         for (i=0; i<LENGTH; i++) {
+        for (i=0; i<LENGTH; i++) {
            MPI_Send(&x[LENGTH*(rang-1)][i], 1, Column_type, rang, 0, MPI_COMM_WORLD);
-         }
-         MPI_Recv(&z[LENGTH*(rang-1)], 1, Column_type, rang, 0, MPI_COMM_WORLD, &status);     
+        }
+	MPI_Recv(&z[rang-1], 1, Column_type, rang, 0, MPI_COMM_WORLD, &status);     
       }
       printf("\nEl proceso %d reunió los elemento: \n", miRango);
       for (i=0; i<LENGTH; 	i++) {
-        for (j=0; j<LENGTH*(numProc-1); j++) {
-          printf(" %d", z[j][i]); 
+        y[0][i] = 0;
+        for (j=0; j<numProc-1; j++) {
+	  y[0][i] = z[j][i] + y[0][i];          
 	}
-      printf("\n\n");	 
-
+        printf(" %d", y[0][i]);
+        printf("\n\n");	 
       }
    } else {
       for(i=0; i<LENGTH; i++){
+        y[0][i] = 0;
         MPI_Recv(&x[0][i], 1, Column_type, 0, 0, MPI_COMM_WORLD, &status);
+        for(j=0; j<LENGTH; j++){
+           y[0][i] = x[j][i] + y[0][i];
+        }
       }
-      MPI_Send(&x, 1, Column_type, 0, 0, MPI_COMM_WORLD);
-/*      printf("\nEl proceso %d ha recibido el elemento: \n", miRango);
-         for (i=0; i<LENGTH; i++) {
-           for (j=0; j<LENGTH; j++) {
-                printf(" %d", x[j][i]);
-           }
-           printf("\n\n");
-         }*/
+      MPI_Send(&y, 1, Column_type, 0, 0, MPI_COMM_WORLD);
    }
 
    MPI_Finalize();
