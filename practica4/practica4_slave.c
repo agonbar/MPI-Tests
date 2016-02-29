e49315feff97e3e87b5a4ff0a528d95038d87685
Adrian Gonzalez Barbosa <adrian.gonzalez.barbosa@gmail.com>
2016-02-29 17:57:56 +0000

    Arreglo en slave para ser dinamico

--- practica4/practica4_slave.c
+++ practica4/practica4_slave.c
@@ -6,23 +6,25 @@
 #define TAG_RESULT 0
 
 main(int argc, char* argv[]) {
-
-   int fila[6], vector[6];
+   
    int result;
-   int i,j;
+   int i,j,hijos;
 
    MPI_Comm intercom;
 
    MPI_Init(&argc, &argv);
 
    MPI_Comm_get_parent(&intercom);
+   
+   MPI_Comm_size(intercom, &hijos);
+   int fila[hijos], vector[hijos];
 
-   MPI_Recv(fila,6,MPI_INT,MPI_ANY_SOURCE,TAG_MATRIZ,intercom,MPI_STATUS_IGNORE);
-   MPI_Recv(vector,6,MPI_INT,MPI_ANY_SOURCE,TAG_VECTOR,intercom,MPI_STATUS_IGNORE);
+   MPI_Recv(fila,hijos,MPI_INT,MPI_ANY_SOURCE,TAG_MATRIZ,intercom,MPI_STATUS_IGNORE);
+   MPI_Recv(vector,hijos,MPI_INT,MPI_ANY_SOURCE,TAG_VECTOR,intercom,MPI_STATUS_IGNORE);
 
    //Cálculo
    result=0;
-   for (i=0;i<6;i++)
+   for (i=0;i<hijos;i++)
       result += vector[i]*fila[i];
 
    MPI_Send(&result,1,MPI_INT,0,TAG_RESULT,intercom);
